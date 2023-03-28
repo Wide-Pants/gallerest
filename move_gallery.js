b969006e2bf059d204d8df06e71ac0b7642a4d13
yunho <ajtwoddl1236@naver.com>
2023-03-28 22:08:07 +0900

    23.03.26

--- move_gallery.js
+++ move_gallery.js
@@ -44,7 +44,7 @@ let pic_H = [[245, 275, 290, 250, 350],
 const image_array=["url(\"https://www.gannett-cdn.com/presto/2019/12/13/PREN/056dc099-5c18-47af-a416-435f29175b70-Switch_PokemonSwordPokemonShield_artwork_01_png_jpgcopy.jpg?width=1320&height=850&fit=crop&format=pjpg&auto=webp\")",
 "url(\"https://sportshub.cbsistatic.com/i/2022/09/07/9ccb5a62-149c-4727-9fca-114923aa0094/pokemonsv-featureart2-c-rgb.jpg\")",
 "url(\"https://sm.ign.com/ign_ap/gallery/a/all-terast/all-terastallized-pokemon_v3tp.jpg\")",
-"url(\"https://pbs.twimg.com/media/DqGBrNdUwAAG_Ja.jpg:large\")",
+"url(\"https://a-static.besthdwallpaper.com/pokemon-party-wallpaper-3000x2000-17797_42.jpg\")",
 "url(\"https://assets.reedpopcdn.com/Pokemon-Scarlet-and-Violet-Header_f1zO36s.jpg/BROK/thumbnail/1200x1200/quality/100/Pokemon-Scarlet-and-Violet-Header_f1zO36s.jpg\")",
 "url(\"https://media2.fdncms.com/rochester/imager/video-game-review-pokemon-xy/u/original/2295145/yveltal_official_illustration_300dpi.jpg\")",
 ];
@@ -59,7 +59,7 @@ for (let a = 0; a < 6; a++) {
     pic_Array[a].style.backgroundImage = image_array[current];
 }
 
-function move_anime(i) {
+function scrolling_up(mv_duration,i){
     for (let b = 0; b < 6; b++) {
         dum_Array[b].style.width = `${pic_W[b][i]+SIZEUP}px`;
         dum_Array[b].style.height = `${pic_H[b][i]+SIZEUP}px`;
@@ -76,7 +76,7 @@ function move_anime(i) {
                     top: `${pic_Y[b][i]-SIZEUP}px`
                 }
             ], {
-                duration: 1000,
+                duration: mv_duration,
                 fill: "forwards",
                 easing: "ease-out"
             });
@@ -89,7 +89,7 @@ function move_anime(i) {
                 top:`${pic_H[b][current]+1200}px`
             }
         ], {
-            duration: 1000,
+            duration: mv_duration,
             fill: "forwards",
             easing: "ease-out"
         });
@@ -100,7 +100,7 @@ function move_anime(i) {
                 left:`${pic_X[b][i]-SIZEUP}px`,
                 top:`${pic_Y[b][i]-SIZEUP}px`,
                 backgroundImage: image_array[i]
-                
+
             }],{
                 duration: 0,
                 fill: "forwards",
@@ -113,7 +113,67 @@ function move_anime(i) {
                 duration: 0,
                 fill: "forwards",
             });
-        },1000)
+        },mv_duration)
+    }
+}
+function move_anime(i) {
+    if(i>current)
+    s;
+    else//scroll up
+        for (let b = 0; b < 6; b++) {
+            dum_Array[b].style.width = `${pic_W[b][i]+SIZEUP}px`;
+            dum_Array[b].style.height = `${pic_H[b][i]+SIZEUP}px`;
+            dum_Array[b].style.left = `${pic_X[b][i]-SIZEUP}px`;
+            dum_Array[b].style.top = "-400px";
+            dum_Array[b].style.backgroundImage = image_array[i]
+            {
+                dum_Array[b].animate([
+                    {
+                        display: `block`,
+                        top: `${pic_H[b][i]+1150}px`
+                    },
+                    {
+                        top: `${pic_Y[b][i]-SIZEUP}px`
+                    }
+                ], {
+                    duration: 900,
+                    fill: "forwards",
+                    easing: "ease-out"
+                });
+            }
+            pic_Array[b].animate([
+                {
+                    top: `${pic_Y[b][current]-SIZEUP}px`
+                },
+                {
+                    top:`${-pic_H[b][current]-300}px`
+                }
+            ], {
+                duration: 900,
+                fill: "forwards",
+                easing: "ease-out"
+            });
+            setTimeout(()=>{
+                pic_Array[b].animate([{
+                    width:`${pic_W[b][i]+SIZEUP}px`,
+                    height:`${pic_H[b][i]+SIZEUP}px`,
+                    left:`${pic_X[b][i]-SIZEUP}px`,
+                    top:`${pic_Y[b][i]-SIZEUP}px`,
+                    backgroundImage: image_array[i]
+                    
+                }],{
+                    duration: 0,
+                    fill: "forwards",
+                })
+                dum_Array[b].animate([
+                    {
+                        display: `none`,
+                    }
+                ], {
+                    duration: 0,
+                    fill: "forwards",
+                });
+            },900)
     }current = i;
 }
 export { move_anime };
